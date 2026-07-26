a44a6465f7c37ef6a95e26719e026d39b2d92848
bilfred <bilfredraju123@gmail.com>
2026-07-26 19:50:10 +0530

    Add GitHub Actions CI pipeline

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,7 +1,8 @@
+import os
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/ask"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000/ask")
 
 st.set_page_config(
     page_title="Enterprise Hybrid RAG",
